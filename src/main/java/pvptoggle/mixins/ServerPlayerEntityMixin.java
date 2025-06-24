15cfb26446dd5a1cde132ebd1c11a54159bc42d9
Drex <nicknamedrex@gmail.com>
2025-06-24 18:20:24 +0200

    Fix mixin

--- src/main/java/pvptoggle/mixins/ServerPlayerEntityMixin.java
+++ src/main/java/pvptoggle/mixins/ServerPlayerEntityMixin.java
@@ -3,7 +3,6 @@ package pvptoggle.mixins;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,8 +13,8 @@ import pvptoggle.PvpWhitelist;
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin extends PlayerEntity {
 
-    public ServerPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile gameProfile) {
-        super(world, pos, yaw, gameProfile);
+    public ServerPlayerEntityMixin(World world, GameProfile profile) {
+        super(world, profile);
     }
 
     @Inject(method = "shouldDamagePlayer", at = @At("HEAD"), cancellable = true)
